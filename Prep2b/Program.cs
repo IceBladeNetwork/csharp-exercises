40150480e230288d4dda1854fb1cad9a5a4c7487
TheIceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-05-09 21:22:27 -0500

    Done with prep work 2

---
+++ Prep2b/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prep2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "Hello",
+                                                      "Cake",
+                                                      "Orange",
+                                                      "wish",
+                                                      "asdfk",
+                                                      "sharp",
+                                                      "clock" };
+            FiveLetterWord(words);
+            Console.ReadLine();
+        }
+
+        static void FiveLetterWord(List<string> words)
+        {
+            foreach (string word in words)
+            {
+                if (word.Length == 5)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
---
+++ Prep2d/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prep2d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.Write("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    // Get the student's grade
+                    Console.Write("grade: ");
+                    int newId = Int32.Parse(Console.ReadLine());
+
+                    students.Add(newId, newStudent);
+                }
+            }
+            while (newStudent != "");
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Value + " (" + student.Key + ")");
+            }
+            Console.ReadLine();
+        }
+    }
+}
