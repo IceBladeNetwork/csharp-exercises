80b293b57c51889d85d9532411f85330e56e4b15
IceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-05-11 18:56:24 -0500

    Done studio 2

---
+++ Studio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Studio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            for (int i = 0; i < sent.Length; i++)
+            {
+                if (!counts.ContainsKey(sent[i]))
+                {
+                    counts.Add(sent[i], 1);
+                } else
+                {
+                    counts[sent[i]]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in counts)
+            {
+                Console.WriteLine(count.Key + ": " + count.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
