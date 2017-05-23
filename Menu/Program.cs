9972fb620fe2c35fe0ae94a93dd4124bc8de334b
IceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-05-22 19:06:24 -0500

    studio 5

---
+++ Menu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class MenuItem 
+    {
+        float price { get; set; }
+        string desc { get; set; };
+        static string catagory { get; set; }
+        public DateTime dateAdded;
+        bool isNew;
+    }
+
+    class Menu
+    {
+        public DateTime lastUpdated;
+        public 
+    }
+}
